a157fddb4ea1b324b2442ded0f697a42f7731b43
Niklas Mähler <niklas.mahler@gmail.com>
2025-09-17 13:10:31 +0200

    Add simple sensor test

---
+++ tests/test_sensor_illumina_directory_sensor.py
@@ -0,0 +1,64 @@
+import mock
+import os
+from pathlib import Path
+from st2tests.base import BaseSensorTestCase
+import tempfile
+
+from illumina_directory_sensor import IlluminaDirectorySensor
+
+
+class IlluminaDirectorySensorTestCase(BaseSensorTestCase):
+    sensor_cls = IlluminaDirectorySensor
+
+    def setUp(self):
+        super(IlluminaDirectorySensorTestCase, self).setUp()
+
+        self.watch_directories = [
+            tempfile.TemporaryDirectory(),
+            tempfile.TemporaryDirectory(),
+        ]
+
+        self.sensor = self.get_sensor_instance(
+            config={
+                "base_url": "http://localhost:8080",
+                "illumina_directories": [Path(d.name) for d in self.watch_directories],
+            }
+        )
+
+    def assert_trigger_dispatched(self, trigger, payload):
+        for t in self.get_dispatched_triggers():
+            if t["trigger"] == trigger:
+                subset_payload = {k: t["payload"].get(k) for k in payload}
+                if subset_payload == payload:
+                    break
+        else:
+            raise AssertionError(
+                f"trigger {trigger} with payload {payload} not dispatched"
+            )
+
+    def test_new_directory(self):
+        registered_dirs = []
+        self.sensor.registered_paths = mock.Mock(return_value=registered_dirs)
+
+        # No triggers with no new directories
+        self.sensor.poll()
+        triggers = self.get_dispatched_triggers()
+        assert len(triggers) == 0
+
+        # Create new "run"
+        new_dir_path = os.path.join(self.watch_directories[0].name, "run1")
+        os.mkdir(new_dir_path)
+
+        # Single trigger should be dispatched
+        self.sensor.poll()
+        triggers = self.get_dispatched_triggers()
+        assert len(triggers) == 1
+        self.assert_trigger_dispatched("cleve.new_directory", {"path": new_dir_path})
+
+        # Add run to database
+        registered_dirs.append(new_dir_path)
+
+        # No new triggers should be dispatched
+        self.sensor.poll()
+        triggers = self.get_dispatched_triggers()
+        assert len(triggers) == 1
